c057ebe6a8b2e6cdcebe0be8504126ef9b267cb6
André <andre@dev-next.com>
2020-05-01 18:49:53 -0300

    :sparkles: Added login page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,13 +1,56 @@
 <template>
-  <q-page></q-page>
+  <q-page>
+    <div class="q-md">
+      <div class="column justify-between">
+        <div class="col">
+          <l-carousel class="login-carousel" />
+        </div>
+        <div class="col justify-center flex">
+          <l-btn-container />
+        </div>
+      </div>
+    </div>
+    <div class="slice"></div>
+  </q-page>
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex';
+import lCarousel from '../components/Login/Carousel.vue';
+import lBtnContainer from '../components/Login/BtnContainer.vue';
 
+export default {
+  components: {
+    lBtnContainer,
+    lCarousel,
+  },
+
+  watch: {
+    pageColor() {
+      this.$emit('changeColor', this.pageColor);
+    },
+  },
+
+  computed: {
+    ...mapState('login', ['pageColor']),
+  },
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+  .login-carousel {
+    height: 70vh;
+  }
 
+  .slice {
+    z-index: 1;
+    background: $grey-1;
+    opacity: .7;
+    height: 33vh;
+    width: 100vw;
+    position: absolute;
+    clip-path: polygon(0 0, 0% 100%, 250% 100%);
+    bottom: 0;
+    left: 0;
+  }
 </style>
