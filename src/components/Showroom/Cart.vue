bee2393b1691a9f30709ddedfe4423646283c74e
André <andre@dev-next.com>
2020-05-03 01:40:27 -0300

    :construction: Added base for cart page

--- src/components/Showroom/Cart.vue
+++ src/components/Showroom/Cart.vue
@@ -2,13 +2,79 @@
   <div class="fit">
     <div class="q-pa-sm cart-page">
       <div class="text-h5 cart-title q-mt-md">Meu Carrinho</div>
+
+      <q-scroll-area style="height: 75vh" class="q-mb-sm">
+        <div class="cart-items q-mt-lg">
+            <s-cart-item
+              class="q-mb-md"
+              v-for="i in cart"
+              :key="i.id"
+              v-bind="i"
+            />
+        </div>
+      </q-scroll-area>
+      <div class="total q-pr-md q-mb-sm">
+        Total: R$ 100,00
+      </div>
+      <q-btn size="lg" color="accent"> Continuar </q-btn>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import sCartItem from './CartItem.vue';
 
+export default {
+  components: {
+    sCartItem,
+  },
+
+  data: () => ({
+    cart: [
+      {
+        id: '1',
+        url: 'https://via.placeholder.com/500',
+        name: 'Nome do Produto',
+        price: 79.99,
+        quantity: 2,
+      },
+      {
+        id: '2',
+        url: 'https://via.placeholder.com/500',
+        name: 'Nome do Produto',
+        price: 79.99,
+        quantity: 1,
+      },
+      {
+        id: '3',
+        url: 'https://via.placeholder.com/500',
+        name: 'Nome do Produto',
+        price: 79.99,
+        quantity: 3,
+      },
+      {
+        id: '3',
+        url: 'https://via.placeholder.com/500',
+        name: 'Nome do Produto',
+        price: 79.99,
+        quantity: 3,
+      },
+      {
+        id: '3',
+        url: 'https://via.placeholder.com/500',
+        name: 'Nome do Produto',
+        price: 79.99,
+        quantity: 3,
+      },
+      {
+        id: '3',
+        url: 'https://via.placeholder.com/500',
+        name: 'Nome do Produto',
+        price: 79.99,
+        quantity: 3,
+      },
+    ],
+  }),
 };
 </script>
 
@@ -16,8 +82,14 @@ export default {
 .cart-page {
   display: flex;
   justify-content: center;
+  flex-direction: column;
   .cart-title {
     text-align: center;
+    font-weight: bold;
+  }
+  .total {
+    width: 100%;
+    text-align: right;
   }
 }
 </style>
